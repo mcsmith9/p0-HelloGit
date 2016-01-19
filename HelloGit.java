1f5fe184c1b202d48e3f648af57b338fae366764
mcsmith9 <mcsmith9@uncg.edu>
2016-01-18 21:48:38 -0500

    changed to hello git

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,5 @@
 /**
- * Author: Jeff Whitworth
+ * Author: Matthew Smith (Copied over from Jeff Whitworth)
  * Program 0 - HelloGit
  * CSC230-02 Spring 2016
  */
@@ -9,7 +9,7 @@ public class HelloGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //outputs the string "Hello World!" to the console
+        //outputs the string "Hello Git!" to the console
         System.out.println("Hello Git!");
     }
     
